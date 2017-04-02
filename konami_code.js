dfd642aadd1498ec968b12c3d36621ac639c9c5d
Divya Lala <divya.h.lala@gmail.com>
2017-04-02 21:22:01 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
+function init(){
+  const input = document.querySelector('body')
+  var i = 0
+  input.addEventListener('keydown',function(e){
+    var key = parseInt(e.which || e.detail)
+    if (key === code[i]) {
+      i++
+      if (i === code.length-1) {
+        alert('You entered the Konami code!')
+        i = 0
+      }
+    } else {
+      i = 0
+    }
+  })
+  return i
 }
+
+//init()
